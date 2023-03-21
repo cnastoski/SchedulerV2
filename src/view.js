3b54f10d93e4805b942e55911883388d513a508e
nnastoski0 <nnastoski1@gmail.com>
2023-03-21 16:59:43 -0400

    add javascript pages

--- src/view.js
+++ src/view.js
@@ -1,22 +1,22 @@
 const fs = require('fs')
+const path = require('path')
 
 function renderHome () {
     const body = document.getElementById("body-container");
-    fs.readFile('templates/main.html', (err, data) => {
+    fs.readFile(path.resolve(__dirname, "main.html"), (err, data) => {
         body.innerHTML = data
-        console.log(data)
     })
 }
 
 function renderSettings () {
     const body = document.getElementById("body-container");
-    fs.readFile('templates/settings.html', (err, data) => {
+    fs.readFile(path.resolve(__dirname, "settings.html"), (err, data) => {
         body.innerHTML = data
-        console.log(data)
     })
+
 }
 
-const re = document.getElementById("homeButton")
+const homeButton = document.getElementById("homeButton")
 homeButton.addEventListener("click", renderHome)
 
 const settingsButton = document.getElementById("settingsButton")
