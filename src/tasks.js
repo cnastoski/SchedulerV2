e792c7aa209d6560b9f8756499d98cd1d0bcacf3
nnastoski0 <nnastoski1@gmail.com>
2023-03-23 10:13:41 -0400

    add rudimentary tasks page

---
+++ src/tasks.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+
+/** TODO:
+ *  implement addTasks + removeTasks function & button
+ *
+ */
+
+export function renderTasks(document) {
+    // pass in document instead of using the document global
+    const tasksBody = document.getElementById("tasks")
+
+    // read tasks file, will most likely get replaced by database lookup
+    // easy to port to database, both functions will be callbacks
+    fs.readFile(path.resolve(__dirname, "tasks.json"), (err, data) => {
+        let html = ""
+        console.log(JSON.parse(data))
+        JSON.parse(data).forEach((task) => {
+            html += "<div class='task'>" + task.name + "<div class='task-sub'>"
+                + task.date + "</div></div><br>"
+        })
+
+
+        tasksBody.innerHTML = html
+    })
+
+}
+
--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+import {renderTasks} from "./tasks.js";
+
 const fs = require('fs')
 const path = require('path')
 
@@ -16,8 +18,23 @@ function renderSettings () {
 
 }
 
+function renderTasksPage () {
+    const body = document.getElementById("body-container");
+    fs.readFile(path.resolve(__dirname, "tasks.html"), (err, data) => {
+        body.innerHTML = data
+        renderTasks(document)
+        // fs.readFile is an async function,
+        // so renderTasks() gets run only during the callback
+    })
+}
+
+
+// consider condensing this or changing system for it
 const homeButton = document.getElementById("homeButton")
 homeButton.addEventListener("click", renderHome)
 
 const settingsButton = document.getElementById("settingsButton")
 settingsButton.addEventListener("click", renderSettings)
+
+const tasksPageButton = document.getElementById("tasksPageButton")
+tasksPageButton.addEventListener("click", renderTasksPage)
