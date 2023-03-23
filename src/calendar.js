e73a815afdd4277310b6ffdfd15109f68e7bf829
Chris <nastoski@msu.edu>
2023-03-23 17:31:35 -0400

    refactor how the calendar renders

--- src/calendar.js
+++ src/calendar.js
@@ -1,8 +1,23 @@
 export function renderCalendar() {
+    renderDays();
+    renderWeekdays();
+}
 
+export function renderWeekdays(){
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const weekdaydiv = document.querySelector("#daynames")
+
+    for (let i in weekday){
+        weekdaydiv.insertAdjacentHTML("beforeend", `<div class="weekday">${weekday[i]}</div>`)
+    }
+
+}
+
+export function renderDays(){
     const calendar = document.querySelector("#calendar")
 
-    for (let day = 1; day <= 31; day++) {
+    for (let day = 1; day <= 35; day++) {
+
         calendar.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
     }
 }
